9f4adaf872397308de3048a5d68a11598e4db27a
suhriar <mochamad.suhri@gmail.com>
2024-04-09 20:49:59 +0700

    gRPC unary

---
+++ calculator/client/sum.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/suhriar/go-grpc-project/calculator/proto"
+)
+
+func doSum(c pb.CalculatorServiceClient) {
+	log.Println("doSum was invoked")
+	res, err := c.Sum(context.Background(), &pb.SumRequest{
+		FirstNumber:  1,
+		SecondNumber: 1,
+	})
+
+	if err != nil {
+		log.Fatalf("Could not sum %v", err)
+	}
+
+	log.Printf("Sum: %d\n", res.Result)
+}
---
+++ greet/client/greet.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/suhriar/go-grpc-project/greet/proto"
+)
+
+func doGreet(c pb.GreetServcieClient) {
+	log.Println("doGreet was invoked")
+	res, err := c.Greet(context.Background(), &pb.GreetRequest{
+		FirstName: "Suhri",
+	})
+
+	if err != nil {
+		log.Fatalf("Could not greet: %v\n", err)
+	}
+
+	log.Printf("Greeting: %s\n", res.Result)
+}
